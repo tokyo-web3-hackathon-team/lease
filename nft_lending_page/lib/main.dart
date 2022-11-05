0d5de2489a7e080591c4a1635e737e16410c174f
rei1011 <r.mizobuchi3@gmail.com>
2022-11-05 14:16:46 +0900

    テーマカラーをブラックに変更

--- nft_lending_page/lib/main.dart
+++ nft_lending_page/lib/main.dart
@@ -12,7 +12,7 @@ class NftLendingApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData.dark(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'NFT LENDING'),
     );
   }
 }
@@ -27,6 +27,12 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: false,
         title: Text(title),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Center(child: Text("Account")),
+          )
+        ],
       ),
       body: Center(
         child: Column(
