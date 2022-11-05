d081bedc678d7311476966a5f4ad28cb2e32579e
rei1011 <r.mizobuchi3@gmail.com>
2022-11-05 14:23:40 +0900

    ホーム画面を切り出し

---
+++ nft_lending_page/lib/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(title),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Center(child: Text("Account")),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- nft_lending_page/lib/main.dart
+++ nft_lending_page/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(const NftLendingApp());
 }
@@ -12,38 +14,7 @@ class NftLendingApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData.dark(),
-      home: const MyHomePage(title: 'NFT LENDING'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text(title),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: Center(child: Text("Account")),
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
+      home: const HomePage(title: 'NFT LENDING'),
     );
   }
 }
