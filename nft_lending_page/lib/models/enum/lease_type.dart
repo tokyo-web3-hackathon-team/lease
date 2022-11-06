05f507b1c840decee8e488bb0cb4a786a4cd8cf4
rei1011 <r.mizobuchi3@gmail.com>
2022-11-06 20:30:33 +0900

    mypageにBORROWとLENDを選択するボタンを作成

--- nft_lending_page/lib/models/enum/lease_type.dart
+++ nft_lending_page/lib/models/enum/lease_type.dart
@@ -1,4 +1,10 @@
 enum LeaseType {
-  borrow,
-  lend,
+  borrow("BORROW"),
+  lend("LEND");
+
+  final String display;
+  const LeaseType(this.display);
+
+  @override
+  String toString() => "The $name leaseType is $display F.";
 }
--- nft_lending_page/lib/pages/my_page.dart
+++ nft_lending_page/lib/pages/my_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nft_lending_page/components/app_bar.dart' as app;
 import 'package:nft_lending_page/components/customized_text_form_field.dart';
 import 'package:nft_lending_page/components/primary_button.dart';
 import 'package:nft_lending_page/constants.dart';
+import 'package:nft_lending_page/models/enum/lease_type.dart';
 import 'package:nft_lending_page/providers.dart';
 
 class MyPage extends HookConsumerWidget {
@@ -13,31 +15,78 @@ class MyPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final walletConnectUrl = useState("");
+    final leaseType = useState(LeaseType.lend);
 
-    return Column(children: [
-      const app.AppBar(),
-      const SizedBox(height: 20),
-      Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: AppConst.padding * 2,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: CustomizedTextFormField(
-                hintText: "Wallet Connect URI",
-                width: 400,
-                onChanged: (e) => walletConnectUrl.value = e,
+    return Scaffold(
+      body: Column(
+        children: [
+          const app.AppBar(),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: AppConst.padding * 2,
+            ),
+            child: PrimaryButton(
+              "connect to Dapps",
+              onPressed: () {
+                ref
+                    .watch(walletProvider.notifier)
+                    .connectDapps(walletConnectUrl.value);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NftsList extends HookConsumerWidget {
+  NftsList({
+    Key? key,
+  }) : super(key: key);
+
+  final leaseType = useState(LeaseType.borrow);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final leaseType = useState(LeaseType.borrow);
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: GestureDetector(
+            onTap: () => leaseType.value = LeaseType.borrow,
+            child: RichText(
+              text: TextSpan(
+                text: LeaseType.borrow.display,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: leaseType.value == LeaseType.borrow
+                      ? Colors.red
+                      : Colors.grey,
+                  decoration: TextDecoration.underline,
+                ),
               ),
             ),
-            PrimaryButton("connect to Dapps", onPressed: () {
-              ref.watch(walletProvider.notifier).connectDapps(walletConnectUrl.value);
-            })
-          ],
+          ),
         ),
-      ),
-    ]);
+        GestureDetector(
+          onTap: () => leaseType.value = LeaseType.lend,
+          child: RichText(
+            text: TextSpan(
+              text: LeaseType.lend.display,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: leaseType.value == LeaseType.lend
+                    ? Colors.red
+                    : Colors.grey,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
