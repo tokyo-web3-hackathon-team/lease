833b875ef7ba7797995a9f958dd9e9ed8fb5c0fb
氏弘一也 <kazuyaujihiro@gmail.com>
2022-11-06 14:41:31 +0900

    add offerProvider

--- nft_lending_page/lib/providers.dart
+++ nft_lending_page/lib/providers.dart
@@ -1,4 +1,8 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nft_lending_page/models/menu/MenuController.dart';
 
+import 'models/OfferController.dart';
+
 final menuProvider = StateNotifierProvider((ref) => MenuController());
+
+final offerProvider = StateNotifierProvider((ref) => OfferController());
